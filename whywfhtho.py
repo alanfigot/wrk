3ec2dae38f398febd9a039c705ad5ad15efcc5b3
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:25:03 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -14,14 +14,14 @@ uploaded_files = st.file_uploader("Choose the 1) Score Key, and 2) Survey Result
 
 # Process the uploaded files
 for uploaded_file in uploaded_files[0:1]:
-	st.subheader('Persona Development _Score Key_ :blue[!!] :sunglasses:')
+	st.subheader(':blue[_Score Key_] File :sunglasses:')
 	if uploaded_file is not None:
 		ScoreKey = pd.read_csv(uploaded_file)
 		st.write(ScoreKey)
 		
 # Process the uploaded files
 for uploaded_file in uploaded_files[1:2]:
-	st.subheader('Persona Development :blue[_Survey Results_] :sunglasses:')
+	st.subheader(':blue[_Survey Results_] File :sunglasses:')
 	if uploaded_file is not None:
 		SurveyResults = pd.read_csv(uploaded_file)
 		st.write(SurveyResults)
