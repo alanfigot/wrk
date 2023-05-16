3de79c36adbe1a5f28ff476b54467ecbfcaa7e86
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 10:49:59 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -8,8 +8,24 @@ If you have any questions...
 
 """
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# Upload the first CSV file
+    st.header("Upload the first CSV file")
+    file_1 = st.file_uploader("Choose a CSV file", type="csv")
+    
+    # Upload the second CSV file
+    st.header("Upload the second CSV file")
+    file_2 = st.file_uploader("Choose a CSV file", type="csv")
 
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write(bytes_data)
+    # Process the uploaded files
+    if file_1 and file_2:
+        df_1 = pd.read_csv(file_1)
+        df_2 = pd.read_csv(file_2)
+
+        # Display the contents of the first CSV file
+        st.subheader("Contents of the first CSV file:")
+        st.write(df_1)
+
+        # Display the contents of the second CSV file
+        st.subheader("Contents of the second CSV file:")
+        st.write(df_2)
+
