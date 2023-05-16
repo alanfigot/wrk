a81080e75398e457fa3974d44daa8c9f2cef8598
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:18:04 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -8,18 +8,19 @@ If you have any questions...
 
 """
 
-# Upload the first CSV file
-st.header("Upload files")
-uploaded_files = st.file_uploader("Choose 2 CSV file containing, 1) the Score Key, and 2) Survey Results", type="csv", accept_multiple_files=True)
+# Upload CSV files
+st.header("Upload CSV files")
+uploaded_files = st.file_uploader("Choose the 1) Score Key, and 2) Survey Results", type="csv", accept_multiple_files=True)
 
 # Process the uploaded files
 for uploaded_file in uploaded_files[0:1]:
 	if uploaded_file is not None:
-		dataframe1= pd.read_csv(uploaded_file)
-		st.write(dataframe1)
+		ScoreKey = pd.read_csv(uploaded_file)
+		st.subheader("Upload CSV files")
+		st.write(ScoreKey)
 		
 # Process the uploaded files
 for uploaded_file in uploaded_files[1:2]:
 	if uploaded_file is not None:
-		dataframe2 = pd.read_csv(uploaded_file)
-		st.write(dataframe2)
+		SurveyResults = pd.read_csv(uploaded_file)
+		st.write(SurveyResults)
