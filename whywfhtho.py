8eec4d275f535dde74583af5c40db6344f3996e8
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:01:39 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -16,5 +16,5 @@ uploaded_files = st.file_uploader("Choose a CSV file", type="csv", accept_multip
 # Process the uploaded files
 for uploaded_file in uploaded_files:
     if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
+    	dataframe = pd.read_csv(uploaded_file)
 	st.write(dataframe)
