571a5a9f83459b2e8902778285e8e2d8d659d41e
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:15:07 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -7,15 +7,19 @@ import streamlit as st
 If you have any questions... 
 
 """
-dataframe1 = []
-dataframe2 = []
 
 # Upload the first CSV file
 st.header("Upload files")
 uploaded_files = st.file_uploader("Choose 2 CSV file containing, 1) the Score Key, and 2) Survey Results", type="csv", accept_multiple_files=True)
 
 # Process the uploaded files
-for i in range(len(uploaded_files)):
-	if uploaded_files[i] is not None:
-	 	dataframe + str(i) = pd.read_csv(uploaded_files[i])
-		st.write(dataframe + str(i))
+for uploaded_file in uploaded_files[0:0]:
+	if uploaded_file is not None:
+		dataframe1= pd.read_csv(uploaded_file)
+		st.write(dataframe1)
+		
+# Process the uploaded files
+for uploaded_file in uploaded_files[1:1]:
+	if uploaded_file is not None:
+		dataframe2 = pd.read_csv(uploaded_file)
+		st.write(dataframe2)
