154430854d91cac70e43e46872b4e61c32700ac2
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:08:17 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -15,7 +15,7 @@ st.header("Upload files")
 uploaded_files = st.file_uploader("Choose 2 CSV file containing, 1) the Score Key, and 2) Survey Results", type="csv", accept_multiple_files=True)
 
 # Process the uploaded files
-for i in range len(uploaded_files):
+for i in range(len(uploaded_files)):
 	print(i)
 	# if uploaded_file is not None:
 	# 	dataframe = pd.read_csv(uploaded_file)
