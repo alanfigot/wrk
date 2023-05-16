523e4f155b9e6ac433f31812ca3b0415d061738a
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 11:22:04 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -13,13 +13,14 @@ st.header("Upload CSV files")
 uploaded_files = st.file_uploader("Choose the 1) Score Key, and 2) Survey Results", type="csv", accept_multiple_files=True)
 
 # Process the uploaded files
-st.subheader('A subheader with _italics_ :blue[colors] and emojis :sunglasses:')
+st.subheader('Persona Development _Score Key_ :blue[!!] :sunglasses:')
 for uploaded_file in uploaded_files[0:1]:
 	if uploaded_file is not None:
 		ScoreKey = pd.read_csv(uploaded_file)
 		st.write(ScoreKey)
 		
 # Process the uploaded files
+st.subheader('Persona Development _Survey Results_ :blue[!!] :sunglasses:')
 for uploaded_file in uploaded_files[1:2]:
 	if uploaded_file is not None:
 		SurveyResults = pd.read_csv(uploaded_file)
