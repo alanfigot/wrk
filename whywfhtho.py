1b06aa8b48b6016cb1074afce12ce0da1b608080
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-16 10:26:33 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -12,12 +12,11 @@ If you have any questions...
 """
 
 upload1 = st.file_uploader("Upload your file here...", type=['csv'])
+upload2 = st.file_uploader("Upload your file here...", type=['csv'])
 
 if upload1 is not None:
 	ScoreKey = pd.read_csv(upload1)
 	st.write(ScoreKey)
-	
-upload2 = st.file_uploader("Upload your file here...", type=['csv'])
 
 if upload2 is not None:
 	SurveyResults = pd.read_csv(upload2)
