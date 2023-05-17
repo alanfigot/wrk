fe82d745c568225039577e89b42b7d5c88e992ca
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-17 16:33:42 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -5,8 +5,6 @@ import numpy as np
 """
 # Persona Development (Not Working Anymore!) 
 
-If you have any questions... 
-
 """
 
 # Create an empty placeholder
@@ -35,11 +33,17 @@ if 'key' in locals():
 	st.write(key)
 		
 # Process the uploaded files
-if 'results' in locals():
+elif 'results' in locals():
         st.subheader(':blue[_Survey Responses_]')
         st.write(results)
 
-if 'results' and 'key' in locals():
+elif 'results' and 'key' in locals():
+	st.subheader(':blue[_Score Key_]')
+	st.write(key)
+	
+	st.subheader(':blue[_Survey Responses_]')
+        st.write(results)
+	
 	results = results.filter(regex='^(F|X)')
 	for i in ['Identifier','IC','SU','DQ','NP','TEAM','FUNC','EXPO','EXPE']: 
 		key[i].fillna('', inplace=True)
@@ -63,25 +67,25 @@ if 'results' and 'key' in locals():
 	st.subheader(':blue[_Analysis Results_] :sunglasses:')
 	st.write(results[['IC','SU','DQ','NP','TEAM','FUNC','EXPO','EXPE']].transpose())
 
-	st.subheader('Filters')
+	# st.subheader('Filters')
 	# Create filter widgets
-	selected_columns = st.multiselect("Select columns", results.columns)
-	selected_rows = st.multiselect("Select rows", results.index)
+	# selected_columns = st.multiselect("Select columns", results.columns)
+	# selected_rows = st.multiselect("Select rows", results.index)
 
 	# Apply filters
-	filtered_df = results[selected_columns].loc[selected_rows]
+	# filtered_df = results[selected_columns].loc[selected_rows]
 
 	# Display filtered DataFrame
-	st.write(filtered_df)
+	# st.write(filtered_df)
 
 	# Create a selectbox widget for column selection
-	selected_column = st.selectbox("Select column for grouping", results.columns)
+	# selected_column = st.selectbox("Select column for grouping", results.columns)
 
 	# Group the DataFrame by the selected column and calculate the average
-	grouped_df = results.groupby(selected_column).mean()
+	# grouped_df = results.groupby(selected_column).mean()
 
 	# Display the grouped DataFrame
-	st.write(grouped_df['IC','SU','DQ','NP','TEAM','FUNC','EXPO','EXPE'])
+	# st.write(grouped_df['IC','SU','DQ','NP','TEAM','FUNC','EXPO','EXPE'])
 
 else:
     placeholder.text("Please upload files")
