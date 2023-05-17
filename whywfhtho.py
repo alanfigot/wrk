bbe38860bb435239e5b8daa455ffde025b2633b0
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-17 16:57:12 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -11,7 +11,7 @@ placeholder = st.empty()
 
 # Upload CSV files
 st.subheader("Please upload the Score Key and Survey Results in CSV format")
-uploaded_files = st.file_uploader('Upload CSV files...", type="csv", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Upload CSV files...", type="csv", accept_multiple_files=True)
 
 if uploaded_files is not None and len(uploaded_files) > 2:
     st.warning("Please upload a maximum of 2 files. Only the first 2 files will be considered.")
