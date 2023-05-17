d6577acda456f92d1a87585a88da90ce6416cfe2
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-17 16:05:03 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -19,6 +19,7 @@ if uploaded_files is not None and len(uploaded_files) > 2:
 for uploaded_file in uploaded_files:
 	df = pd.read_csv(uploaded_file)
 	df = df.rename(columns=lambda x: x.strip(), inplace=True)
+	print(df.columns)
 	if any(string in df.columns for string in ['Identifier','Min','Max']):
 		key = uploaded_file
 	else: 
