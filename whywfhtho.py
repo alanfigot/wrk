3a57e9eca9c60746d9650212f385ba9207051527
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-05-19 11:52:02 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -164,7 +164,7 @@ elif 'results' and 'key' in locals():
 	st.write(totals.style.format("{:.2}"))
 	
 	# Create a selectbox widget for column selection
-	selected_column = st.selectbox("Select column for grouping", key['Filter'].unique())
+	selected_column = st.selectbox("Select column for grouping", key['Filter'].unique().dropna())
 	
 	if 'labels' in locals():
 		labels = labels.filter(regex='^P')
