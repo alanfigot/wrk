5bc06316a45ca440ae743780900c0e27fdb92648
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-06-07 12:52:43 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -21,9 +21,10 @@ if uploaded_files is not None and len(uploaded_files) > 3:
     uploaded_files = uploaded_files[:3]
 
 for uploaded_file in uploaded_files:
-	if uploaded_file.name.endswith.('.csv') == True:
+	filename = uploaded_file.name
+	if filename.endswith.('.csv') == True:
         	df = pd.read_csv(uploaded_file, index_col=False)
-	elif uploaded_file.name.endswith('.xlsx') == True:
+	elif filename.endswith('.xlsx') == True:
 		df = pd.read_excel(io=uploaded_file)
 	df.rename(columns=lambda x: x.strip(), inplace=True)
 	# Identify Score Key File 
