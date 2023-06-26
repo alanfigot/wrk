fe9bb0a355e2cede6348afdca9e88e4f67f7f35d
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-06-26 16:24:38 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -237,9 +237,8 @@ if 'results' in locals() and 'key' in locals():
 
 		# Bar Plot
 		temp = score[[column for column in score.columns if pd.api.types.is_numeric_dtype(score[column])]]
+		temp = score.groupby(variable3_id, as_index=False).mean().sort_values(variable1, ascending=False)
 		st.write(temp)
-		
-		# temp = pd.DataFrame(score.groupby(variable3_id, as_index=False).mean()) # .sort_values(variable1, ascending=False)
 		
 		# if len(score[variable3_id].unique())>10:
 		# 	temp = pd.concat([temp.head(5),temp.tail(5)])
