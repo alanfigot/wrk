fe7ff095e0ab365eead90b473c24fa39d26bff56
alanfigot <36059855+alanfigot@users.noreply.github.com>
2023-06-26 13:49:07 -0700

    Update whywfhtho.py

--- whywfhtho.py
+++ whywfhtho.py
@@ -214,44 +214,40 @@ if 'results' in locals() and 'key' in locals():
 		score = results.join(totals)
 		score.fillna('', inplace=True)
 
-	graphic = st.radio("Select one of the following options:",('Scatter', 'Bar', 'Distribution', 'Box'))
+	# graphic = st.radio("Select one of the following options:",('Scatter', 'Bar', 'Distribution', 'Box'))
 
-	def refresh_plot(graphic, variable1, variable2, variable3):
-		if graphic == "Scatter":
-			scatter_plot(variable1, variable2, variable3)
-		elif graphic == "Bar":
-			pass
-		elif graphic == "Distribution":
-			pass
-		elif graphic == "Box":
-			pass
-		else:
-			pass
-
-	def scatter_plot(variable1, variable2, variable3):
-		if variable1 != '' and variable2 != '' and variable3 != '': 
-			fig = px.scatter(score, x=variable1, y=variable2, color=identifier) 
-			st.plotly_chart(fig, theme='streamlit', use_container_width=True)
-		elif variable1 != '' and variable2 != '':
-			fig = px.scatter(score, x=variable1, y=variable2)
-			# fig.update_layout(title_text=f'{x_axis} Score by {y_axis}')
-			st.plotly_chart(fig, theme='streamlit', use_container_width=True)
-		else:
-			pass
-	
 	variable1 = st.selectbox("Variable 1",list([''] + ['IC', 'SU', 'DQ', 'NP', 'Teamwork','Functionality','Exposure','Experience'])) # on_change=refresh_plot
 	variable2 = st.selectbox("Variable 2",list([''] + ['IC', 'SU', 'DQ', 'NP', 'Teamwork','Functionality','Exposure','Experience'])) # on_change=refresh_plot
-	variable3 = ''
+	variable3 = st.selectbox('Variable 3',list([''] + list(key['Questions'].values)), on_change=refresh_plot)
+	identifier = ""
+	if variable3 != "":
+		identifier = key[key['Questions']=='Please select your generation.']['Identifier'].values[0]
 
-	if graphic != "Bar":
-		variable3 = st.selectbox('Variable 3',list([''] + list(key['Questions'].values)), on_change=refresh_plot)
-		if variable3 != '':
-			identifier = key.loc[key['Questions']=='Please select your business unit.']['Identifier'].values[0]
+	fig1 = px.scatter(score, x=variable1, y=variable2, color=identifier) 
+	fig2 = px.histogram(score, x=variable1, marginal="rug", color=identifier, hover_data=score.columns)
+	
+	tab1, tab2 = st.tabs(["Scatter", "Distribution"])
+	with tab1:
+		st.plotly_chart(fig1, theme='streamlit', use_container_width=True)
+	with tab2:
+		st.plotly_chart(fig2, theme='streamlit', use_container_width=True)
+
+	
+	# def refresh_plot(score, variable1, variable2, variable3):
+	#	if variable1 != '' and variable2 != '' and variable3 != '': 
+	#		fig = px.scatter(score, x=variable1, y=variable2, color=identifier) 
+	#		st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+	#	elif variable1 != '' and variable2 != '':
+	#		fig = px.scatter(score, x=variable1, y=variable2)
+	#		# fig.update_layout(title_text=f'{x_axis} Score by {y_axis}')
+	#		st.plotly_chart(fig, theme='streamlit', use_container_width=True)
+	#	else:
+	#		pass
 
 	button = st.button("Refresh Plot")
 
-	if button:
-		refresh_plot(graphic, variable1, variable2, variable3)
+	#if button:
+	#	refresh_plot(graphic, variable1, variable2, variable3)
 
 	# Download Options
 	st.subheader(':blue[_Download Data_] ')
